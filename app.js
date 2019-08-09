1289d5820cbfe31db3f7b8b00a5ac4996b5c8731
Dawid Bartosz <dawid.bartosz@edulab.io>
2019-08-09 21:31:37 +0200

    wip

--- app.js
+++ app.js
@@ -6,57 +6,73 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const client = require('twilio')(process.env.TWILIO_SID, process.env.AUTH_TOKEN);
+const client = require('twilio')(
+  process.env.TWILIO_SID,
+  process.env.AUTH_TOKEN
+);
 
-const { Translate } = require('@google-cloud/translate'); 
+const { Translate } = require('@google-cloud/translate');
 const translate = new Translate({
-    projectId: process.env.GCP_PROJECT_ID,
-    keyFilename: './VizGov-e98255c0ab5d.json'
+  projectId: process.env.GCP_PROJECT_ID,
+  keyFilename: './VizGov-e98255c0ab5d.json',
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-let unknown_response = "Hello world!";
+let unknown_response = 'Hello world!';
 
 app.get('/unknown_response/:resp', (req, res) => {
-    unknown_response = req.params.resp;
+  unknown_response = req.params.resp;
 
-    client.calls
-        .create({
-            url: `${process.env.TUNNELED_URL}/twilio`,
-            to: "+48785757810",
-            from: process.env.TWILIO_PHONE
-        })
-        .then(call => {
-            res.sendStatus(200);
-        })
-        .catch(err => console.log(err));
+  client.calls
+    .create({
+      url: `${process.env.TUNNELED_URL}/twilio`,
+      to: '+48785757810',
+      from: process.env.TWILIO_PHONE,
+    })
+    .then(call => {
+      res.sendStatus(200);
+    })
+    .catch(err => console.log(err));
 });
 
 app.post('/unknownquestionresponse', async (req, res) => {
-    const speechData = req.body.SpeechResult;
+  const speechData = req.body.SpeechResult;
 
-    const [translations] = await translate.translate(speechData, 'en-US');
+  const [translations] = await translate.translate(speechData, 'en-US');
 
-    res.send(translations);
+  res.send(translations);
 });
 
 app.post('/twilio', async (req, res) => {
-    res.set('Content-Type', 'text/xml');
+  res.set('Content-Type', 'text/xml');
 
-    let [translations] = await translate.translate(unknown_response, 'it'); 
+  let [translations] = await translate.translate(unknown_response, 'it');
 
-    const voiceResponse = `<?xml version="1.0" encoding="UTF-8"?>
+  const voiceResponse = `<?xml version="1.0" encoding="UTF-8"?>
     <Response>
         <Say>${translations}</Say>
-        <Gather input="speech" action="${process.env.TUNNELED_URL}/unknownquestionresponse">
+        <Gather input="speech" action="${
+          process.env.TUNNELED_URL
+        }/unknownquestionresponse">
             <Say>Per favore, rispondi alla domanda</Say>
         </Gather>
     </Response>
     `;
 
-    res.send(voiceResponse);
+  res.send(voiceResponse);
 });
 
-app.listen(3000, () => console.log("Running on 3000!"));
+app.post('/dialogFlow', (req, res) => {
+  const body = req.body;
+  // const intent = body.intent;
+  // const intentName = intent.name;
+  // const params = body.parameters;
+  console.log(body);
+  // console.log(intent);
+  // console.log(params);
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log('Running on 3000!'));
